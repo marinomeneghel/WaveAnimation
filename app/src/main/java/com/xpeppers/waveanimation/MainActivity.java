7db521245ec189f27ceeb8c75a2fbbd9dc88bff1
Marino Meneghel <marino.meneghel@xpeppers.com>
2017-03-15 14:51:19 +0100

    Performed wave animation on views

--- app/src/main/java/com/xpeppers/waveanimation/MainActivity.java
+++ app/src/main/java/com/xpeppers/waveanimation/MainActivity.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class MainActivity extends AppCompatActivity {
 
     private Handler handler;
     private View[] views;
     private double duration = 400;
+    private double stopDelay = 600;
     private double phaseShift = 0.1;
 
     @Override
@@ -27,33 +30,25 @@ public class MainActivity extends AppCompatActivity {
 
         handler = new Handler();
         handler.postDelayed(runnable, (long) (duration * 3));
+
     }
 
     Runnable runnable = new Runnable() {
         @Override
         public void run() {
-            updateTime();
+            for (int i = 0; i < views.length; i++) {
+                final int finalI = i;
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.waves_animation);
+                        views[finalI].startAnimation(animation);
+                    }
+                }, 300 * finalI);
+            }
+            handler.postDelayed(this, (long) (duration * 3 + stopDelay));
         }
     };
 
-    private void updateTime() {
-        for (int i = 0; i < views.length; i++) {
-            animate(views[i], i);
-            handler.postDelayed(runnable, (long) (duration * 3));
-        }
-    }
 
-    private void animate(View view, int index) {
-        scaleUp(view, index);
-        scaleDown(view, index);
-    }
-
-    private void scaleUp(View view, int index) {
-        int delay = (int) ((index * phaseShift) + duration);
-        
-    }
-
-    private void scaleDown(View view, int index) {
-        view.animate().scaleY(10f);
-    }
 }
